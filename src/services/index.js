3f785f2ea9215bbb5518c04e0cc38222b5629b60
Imraan Habiboellah <i.habiboellah@gmail.com>
2016-10-24 19:04:09 +0200

    fix db

--- src/services/index.js
+++ src/services/index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 module.exports = function() {
   const app = this;
 
-  mongoose.connect(app.get('mongodb'));
+  mongoose.connect(process.env.MONGODB_URI || app.get('mongodb'));
   mongoose.Promise = global.Promise;
 
   app.configure(authentication);
